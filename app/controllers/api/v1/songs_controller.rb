fbd1498f0d25d06928af3a752d968d9d8820bc90
Jack O'Neill <joneill.tennis@gmail.com>
2015-09-06 16:30:03 -0400

    destroy

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -11,7 +11,21 @@ module Api
         respond_with Song.find(params[:id])
       end
 
-      
+      def destroy
+
+        # if !@song.cover_art.nil? && @song.cover_art != "public/a/defaultImg.png"
+        #   sl = Song.where(:directory => current_user.user_directory)
+        #   sl1 = sl.select { |i| i.cover_art == @song.cover_art }
+        #   current_user.total_file_size -= File.size("public#{s.file}")
+
+        #   if sl1.count == 1
+        #     current_user.total_file_size -= File.size("public/a/#{s.cover_art}")
+        #     File.delete("public/a/#{s.cover_art}")
+        #   end
+        #end
+        File.delete("public#{@song.file}")
+        @song.destroy
+      end
     end
   end
 end
